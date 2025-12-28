04076f62d332621fc4bf0787bd0fb824933c7ac0
Shah Newaz <shahnewaz385@gmail.com>
2025-12-28 09:39:17 +0600

    All

--- src/app/components/mascowash/setup/entry/receive-operation/receive-operation.component.ts
+++ src/app/components/mascowash/setup/entry/receive-operation/receive-operation.component.ts
@@ -10,12 +10,11 @@ import { WashSetupService } from '../../../services/washsetup.service';
 })
 export class ReceiveOperationComponent implements OnInit {
 
-  // ================= FLAGS =================
+
   isReviewMode = false;
 
-  // ================= MASTER =================
-  Model: any = {
-    unitId: null,
+  Model = {
+    UnitId: null,
     trackingNo: '',
     dyeingBatchNo: '',
     manualNo: '',
@@ -24,17 +23,15 @@ export class ReceiveOperationComponent implements OnInit {
     isManualActive: false
   };
 
-  review: any = {
-    unitId: null,
+  review = {
+    UnitId: null,
     fromDate: null,
     toDate: null
   };
 
-  // ================= GRID =================
   detailList: any[] = [];
 
-  // ================= DDL =================
-  unitList: any[] = [];
+  UnitList: any[] = [];
   fromUnitList: any[] = [];
   buyerList: any[] = [];
   jobList: any[] = [];
@@ -46,7 +43,6 @@ export class ReceiveOperationComponent implements OnInit {
   dressPartList: any[] = [];
   uomList: any[] = [];
 
-  // ================= SIZE POPUP =================
   sizePopupVisible = false;
   selectedRow: any;
   sizeList: any[] = [];
@@ -57,128 +53,269 @@ export class ReceiveOperationComponent implements OnInit {
     private toastr: ToastrService,
     private cdr: ChangeDetectorRef
   ) {}
+isNewMode = false;
+
+
+onNew() {
+  this.isNewMode = true;
+  this.isReviewMode = false;
+}
+
+onReview() {
+  this.isReviewMode = true;
+  this.isNewMode = false;
+}
+
 
   ngOnInit(): void {
     this.loadDropdowns();
+
   }
-
-  // ================= LOAD DDL =================
-  loadDropdowns() {
-    this.service.GetUnitName().subscribe(res => {
-      this.unitList = res.map(x => ({ label: x.DisplayName, value: x.ID }));
-      this.fromUnitList = this.unitList;
-    });
-
-    this.service.GetBuyerNameDDL().subscribe(res => this.buyerList = res);
-    this.service.GetJobNoDDL().subscribe(res => this.jobList = res);
-    this.service.GetStyleNoDDL().subscribe(res => this.styleList = res);
-    this.service.GetOrderNoDDL().subscribe(res => this.orderList = res);
-    this.service.GetFabricationDDL().subscribe(res => this.fabricationList = res);
-    this.service.GetGSMDDL().subscribe(res => this.gsmList = res);
-    this.service.GetDressPartDDL().subscribe(res => this.dressPartList = res);
-    this.service.GetUOMDDL().subscribe(res => this.uomList = res);
-  }
-
-  // ================= PAGE MODE =================
-  onNew() {
-    this.isReviewMode = false;
-    this.clearAll();
-  }
-
-  onReview() {
-    this.isReviewMode = !this.isReviewMode;
-  }
-
-  // ================= TRACKING SEARCH =================
-  onTrackingEnter() {
-    if (!this.Model.trackingNo) {
-      this.toastr.warning('Enter tracking no');
-      return;
-    }
-
-    this.service.getReceiveByTrackingNo(this.Model.trackingNo)
-      .subscribe(res => {
-        if (!res.length) {
-          this.toastr.info('No data found');
-          return;
-        }
-        this.bindDetailRows(res);
+ loadDropdowns() {
+    this.UnitList = [];
+    this.service.GetUnitName().subscribe((res) => {
+      this.UnitList.push({ label: '-- Select --', value: null });
+      res.forEach((x) => {
+        this.UnitList.push({
+          label: x.DisplayName ?? x.displayName,
+          value: x.ID ?? x.id,
+        });
+        this.fromUnitList = [...this.UnitList];
       });
-  }
+    });
+  
 
-  bindDetailRows(rows: any[]) {
-    this.detailList = rows.map(r => ({
-      trackingNo: r.trackingNo,
-      fromUnitId: r.fromUnitId,
-      receiveDate: new Date(r.receiveDate),
-      buyerNo: r.buyerNo,
-      jobId: r.jobId,
-      styleNo: r.styleNo,
-      orderId: r.orderId,
-      type: r.type,
-      fabricationId: r.fabrication,
-      composition: r.composition,
-      gsmId: r.iszid,
-      colorId: r.icleid,
-      dressPartId: r.dressPartId,
-      operationTypes: r.operationType,
-      uomId: r.uomDetailsId,
-      size: r.size,
-      totalQty: r.qty,
-      probableDeliveryDate: new Date(r.probableDeliveryDate),
-      shipmentDate: new Date(r.shipmentDate),
-      sizeDetails: []
+
+  this.service.GetBuyerNameDDL().subscribe(res => {
+    this.buyerList = res.map(x => ({
+      label: x.DisplayName ?? x.displayName,
+          value: x.ID ?? x.id,
+      // label: x.BuyerName,
+      // value: Number(x.BuyerNo)
     }));
+  });
+this.service.GetJobNoDDL().subscribe(res => {
+  this.jobList = res.map(x => ({
+    label: x.JobInfo,
+    value: x.JobId.toString()
+  }));
+});
+  // this.service.GetJobNoDDL().subscribe(res => {
+  //   this.jobList = res.map(x => ({
+  //     label: x.DisplayName ?? x.displayName,
+  //         value: x.ID ?? x.id,
+  //     // label: x.JobInfo,
+  //     // value: Number(x.JobId)
+  //   }));
+  //   console.log("Job List", this.jobList);
+  // });
 
-    this.cdr.detectChanges();
-  }
+  this.service.GetStyleNoDDL().subscribe(res => {
+    this.styleList = res.map(x => ({
+      label: x.DisplayName ?? x.displayName,
+          value: x.ID ?? x.id,
+      // label: x.StyleName,
+      // value: Number(x.StyleNo)
+    }));
+  });
 
-  // ================= SIZE POPUP =================
+  this.service.GetOrderNoDDL().subscribe(res => {
+    this.orderList = res.map(x => ({
+      label: x.DisplayName ?? x.displayName,
+          value: x.ID ?? x.id,
+      // label: x.OrderNo,
+      // value: Number(x.OrderId)
+    }));
+  });
+
+  this.service.GetFabricationDDL().subscribe(res => {
+    this.fabricationList = res.map(x => ({
+       label: x.DisplayName ?? x.displayName,
+          value: x.ID ?? x.id,
+      // label: x.FabricationName,
+      // value: Number(x.Fabrication)
+    }));
+  });
+
+  this.service.GetGSMDDL().subscribe(res => {
+    this.gsmList = res.map(x => ({
+     label: x.DisplayName ?? x.displayName,
+          value: x.ID ?? x.id,
+      // label: x.GSM,
+      // value: Number(x.ISZID)
+    }));
+  });
+
+  this.service.GetDressPartDDL().subscribe(res => {
+    this.dressPartList = res.map(x => ({
+     label: x.DisplayName ?? x.displayName,
+          value: x.ID ?? x.id,
+      // label: x.DressPart,
+      // value: Number(x.DressPartId)
+    }));
+  });
+
+  this.service.GetUOMDDL().subscribe(res => {
+    this.uomList = res.map(x => ({
+     label: x.DisplayName ?? x.displayName,
+          value: x.ID ?? x.id,
+      // label: x.UOM,
+      // value: Number(x.UOMDetailsId)
+    }));
+  });
+}
+
+onTrackingEnter() {
+  this.service.getReceiveByTrackingNo(this.Model.trackingNo)
+    .subscribe(res => this.bindDetailRows(res));
+}
+
+bindDetailRows(rows: any[]) {
+  this.detailList = rows.map(r => ({
+    trackingNo: r.trackingNo,
+    fromUnitId: Number(r.fromUnitId),
+    receiveDate: new Date(r.receiveDate),
+    buyerNo: Number(r.buyerNo),
+     jobId: r.JobId?.toString(),
+    styleNo: Number(r.styleNo),
+    orderId: Number(r.orderId),
+    type: r.type,
+    fabricationId: Number(r.fabrication),
+    composition: r.composition,
+    gsmId: Number(r.iszid),
+    colorId: Number(r.icleid),
+    dressPartId: Number(r.dressPartId),
+    uomId: Number(r.uomDetailsId),
+    totalQty: r.qty,
+    probableDeliveryDate: new Date(r.probableDeliveryDate),
+    shipmentDate: new Date(r.shipmentDate),
+    operationTypes: '',
+    sizeDetails: []
+  }));
+}
+
+
+  // loadDropdowns() {
+  //   this.UnitList = [];
+  //   this.service.GetUnitName().subscribe((res) => {
+  //     this.UnitList.push({ label: '-- Select --', value: null });
+  //     res.forEach((x) => {
+  //       this.UnitList.push({
+  //         label: x.DisplayName ?? x.displayName,
+  //         value: x.ID ?? x.id,
+  //       });
+  //       this.fromUnitList = [...this.UnitList];
+  //     });
+  //   });
+  
+
+  //   this.service.GetBuyerNameDDL().subscribe(res =>
+  //     this.buyerList = res.map(x => ({
+  //       label: x.BuyerName,
+  //       value: x.BuyerNo
+  //     }))
+  //   );
+
+
+
+
+  //   this.service.GetJobNoDDL().subscribe(res =>
+  //     this.jobList = res.map(x => ({
+  //       label: x.JobInfo,
+  //       value: x.JobId
+  //     }))
+  //   );
+
+  //   this.service.GetStyleNoDDL().subscribe(res =>
+  //     this.styleList = res.map(x => ({
+  //       label: x.StyleName,
+  //       value: x.StyleNo
+  //     }))
+  //   );
+
+  //   this.service.GetOrderNoDDL().subscribe(res =>
+  //     this.orderList = res.map(x => ({
+  //       label: x.OrderNo,
+  //       value: x.OrderId
+  //     }))
+  //   );
+
+  //   this.service.GetFabricationDDL().subscribe(res =>
+  //     this.fabricationList = res.map(x => ({
+  //       label: x.FabricationName,
+  //       value: x.Fabrication
+  //     }))
+  //   );
+
+  //   this.service.GetGSMDDL().subscribe(res =>
+  //     this.gsmList = res.map(x => ({
+  //       label: x.GSM,
+  //       value: x.ISZID
+  //     }))
+  //   );
+
+  //   this.service.GetDressPartDDL().subscribe(res =>
+  //     this.dressPartList = res.map(x => ({
+  //       label: x.DressPart,
+  //       value: x.DressPartId
+  //     }))
+  //   );
+
+  //   this.service.GetUOMDDL().subscribe(res =>
+  //     this.uomList = res.map(x => ({
+  //       label: x.UOM,
+  //       value: x.UOMDetailsId
+  //     }))
+  //   );
+  // }
+
+  // onTrackingEnter() {
+  //   this.service.getReceiveByTrackingNo(this.Model.trackingNo)
+  //     .subscribe(res => this.bindDetailRows(res));
+  // }
+
+  // bindDetailRows(rows: any[]) {
+  //   this.detailList = rows.map(r => ({
+  //     trackingNo: r.trackingNo,
+  //     fromUnitId: r.fromUnitId,
+  //     receiveDate: new Date(r.receiveDate),
+  //     buyerNo: r.buyerNo,
+  //     jobId: r.jobId,
+  //     styleNo: r.styleNo,
+  //     orderId: r.orderId,
+  //     type: r.type,
+  //     fabricationId: r.fabrication,
+  //     composition: r.composition,
+  //     gsmId: r.iszid,
+  //     colorId: r.icleid,
+  //     dressPartId: r.dressPartId,
+  //     uomId: r.uomDetailsId,
+  //     totalQty: r.qty,
+  //     probableDeliveryDate: new Date(r.probableDeliveryDate),
+  //     shipmentDate: new Date(r.shipmentDate),
+  //     sizeDetails: []
+  //   }));
+  //   this.cdr.detectChanges();
+  // }
+
   openSizePopup(row: any) {
     this.selectedRow = row;
-    this.sizeList = row.sizeDetails?.length
-      ? JSON.parse(JSON.stringify(row.sizeDetails))
-      : [{ size: row.size, qty: row.totalQty }];
-
+    this.sizeList = row.sizeDetails.length
+      ? [...row.sizeDetails]
+      : [{ size: 'Default', qty: row.totalQty }];
     this.calculateSizeTotal();
     this.sizePopupVisible = true;
   }
 
   calculateSizeTotal() {
-    this.totalSizeQty = this.sizeList.reduce((s, x) => s + (+x.qty || 0), 0);
+    this.totalSizeQty = this.sizeList.reduce((a, b) => a + (+b.qty || 0), 0);
   }
 
   confirmSizeQty() {
-    this.selectedRow.sizeDetails = this.sizeList;
+    this.selectedRow.sizeDetails = [...this.sizeList];
     this.selectedRow.totalQty = this.totalSizeQty;
     this.sizePopupVisible = false;
   }
-
-  // ================= SAVE =================
-  onSave() {
-    if (!this.Model.unitId) {
-      this.toastr.warning('Unit required');
-      return;
-    }
-    if (!this.detailList.length) {
-      this.toastr.warning('No data to save');
-      return;
-    }
-
-    Swal.fire('Ready', 'Save API will be called here', 'success');
-  }
-
-  // ================= CLEAR =================
-  clearAll() {
-    this.Model = {
-      unitId: null,
-      trackingNo: '',
-      dyeingBatchNo: '',
-      manualNo: '',
-      isTrackingActive: true,
-      isDyeingActive: false,
-      isManualActive: false
-    };
-    this.detailList = [];
-  }
 }
+
+
